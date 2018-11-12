efcb42ad540b9b0757de9438385758dfeeceb35f
覃幸和 <qinxinghe@zp2.com>
2018-11-12 16:24:12 +0800

    完成sidebar部分

---
+++ sidebar.php
@@ -0,0 +1,30 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<aside class="sidebar">
+    <section class="widgetbox"><h3>嗯？</h3>
+        <p style="margin-left:auto;margin-right:auto;text-align:center;font-family:Microsoft Yahei, Helvetica, Arial, sans-serif;font-size:13px;">
+            是要<a href="#">登陆</a>呀？ </p>
+    </section>
+    <section class="widgetbox">
+        <h3>网站统计</h3>
+        <ul class="blogroll clearfix">
+            <li>文章总数：115篇</li>
+            <li>评论总数：673条</li>
+            <li>页面总数：15 个</li>
+            <li>分类总数：4个</li>
+            <li>标签总数：201个</li>
+            <li>网站运行：1347天</li>
+        </ul>
+    </section>
+    <section class="widgetbox"><h3>友情链接</h3>
+        <div class="textwidget">
+            <ul class="friendlinks">
+                <li><a href="#" target="_blank" title="Exploring the Tao"> Pret的121号实验室</a></li>
+                <li><a href="#" target="_blank" title="Exploring the Tao"> Pret的121号实验室</a></li>
+                <li><a href="#" target="_blank" title="Exploring the Tao"> Pret的121号实验室</a></li>
+                <li><a href="#" target="_blank" title="Exploring the Tao"> Pret的121号实验室</a></li>
+                <li><a href="#" target="_blank" title="Exploring the Tao"> Pret的121号实验室</a></li>
+                <li><a href="#" style="color:red;">·查看更多OR申请</a></li>
+            </ul>
+        </div>
+    </section>
+</aside>
