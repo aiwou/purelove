e9ffa8746bc61e96c933f01e314102ad0bda297d
覃幸和 <qinxinghe@zp2.com>
2018-11-12 16:23:58 +0800

    完成footer部分

---
+++ footer.php
@@ -0,0 +1,122 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?></article><!-- article -->
+</section><!-- #container -->
+<span class="clearfix"></span>
+<div id="bak_top"></div>
+</div>
+<footer class="footer">
+        <div class="inner clearfix">
+            <div class="fotbox">
+                <h3>标签云</h3>
+                <a href='https://www.azimiao.com/tag/technology' class='tag-link-45 tag-link-position-1' title='41个话题'
+                   style='font-size: 12px;'>技术</a>
+                <a href='https://www.azimiao.com/tag/essay' class='tag-link-105 tag-link-position-2' title='37个话题'
+                   style='font-size: 12px;'>随笔</a>
+                <a href='https://www.azimiao.com/tag/impress' class='tag-link-101 tag-link-position-3' title='36个话题'
+                   style='font-size: 12px;'>印象</a>
+                <a href='https://www.azimiao.com/tag/technology_code' class='tag-link-56 tag-link-position-4' title='28个话题'
+                   style='font-size: 12px;'>技术·编程</a>
+                <a href='https://www.azimiao.com/tag/%e5%9b%be%e5%83%8f' class='tag-link-30 tag-link-position-5'
+                   title='20个话题' style='font-size: 12px;'>图像</a>
+                <a href='https://www.azimiao.com/tag/impress_days' class='tag-link-104 tag-link-position-6' title='19个话题'
+                   style='font-size: 12px;'>印象·日常</a>
+                <a href='https://www.azimiao.com/tag/essay_other' class='tag-link-106 tag-link-position-7' title='17个话题'
+                   style='font-size: 12px;'>随笔·杂谈</a>
+                <a href='https://www.azimiao.com/tag/%e6%96%87%e7%ab%a0' class='tag-link-48 tag-link-position-8'
+                   title='15个话题' style='font-size: 12px;'>文章</a>
+                <a href='https://www.azimiao.com/tag/%e5%9b%be%e5%83%8f%c2%b7%e6%97%a5%e5%b8%b8'
+                   class='tag-link-50 tag-link-position-9' title='12个话题' style='font-size: 12px;'>图像·日常</a>
+                <a href='https://www.azimiao.com/tag/c' class='tag-link-69 tag-link-position-10' title='11个话题'
+                   style='font-size: 12px;'>c#</a>
+                <a href='https://www.azimiao.com/tag/impress_game' class='tag-link-102 tag-link-position-11' title='11个话题'
+                   style='font-size: 12px;'>印象·游戏</a>
+                <a href='https://www.azimiao.com/tag/unity' class='tag-link-197 tag-link-position-12' title='11个话题'
+                   style='font-size: 12px;'>unity</a>
+                <a href='https://www.azimiao.com/tag/technology_software' class='tag-link-47 tag-link-position-13'
+                   title='10个话题' style='font-size: 12px;'>技术·软件</a>
+                <a href='https://www.azimiao.com/tag/essay_days' class='tag-link-108 tag-link-position-14' title='9个话题'
+                   style='font-size: 12px;'>随笔·日常</a>
+                <a href='https://www.azimiao.com/tag/%e6%96%87%e7%ab%a0%c2%b7%e6%9d%82%e8%b0%88'
+                   class='tag-link-52 tag-link-position-15' title='8个话题' style='font-size: 12px;'>文章·杂谈</a>
+                <a href='https://www.azimiao.com/tag/technology_other' class='tag-link-53 tag-link-position-16' title='8个话题'
+                   style='font-size: 12px;'>技术·杂谈</a>
+                <a href='https://www.azimiao.com/tag/wordpress' class='tag-link-190 tag-link-position-17' title='8个话题'
+                   style='font-size: 12px;'>wordpress</a>
+                <a href='https://www.azimiao.com/tag/essay_animation' class='tag-link-107 tag-link-position-18' title='7个话题'
+                   style='font-size: 12px;'>随笔·动漫</a>
+                <a href='https://www.azimiao.com/tag/%e6%96%87%e7%ab%a0%c2%b7%e5%8a%a8%e6%bc%ab'
+                   class='tag-link-49 tag-link-position-19' title='6个话题' style='font-size: 12px;'>文章·动漫</a>
+                <a href='https://www.azimiao.com/tag/impress_animation' class='tag-link-109 tag-link-position-20'
+                   title='6个话题' style='font-size: 12px;'>印象·动漫</a></div>
+            <div class="fotbox link">
+                <h3>版权声明</h3>
+                1.您可自由分发和演绎本站内容，只需保留本站署名且非商业使用(CC BY-NC-SA 3.0 CN)。<br/>
+                2.本站引用资源会尽最大可能标明出处及著作权所有者，但不能保证对所有资源都可声明上述内容。侵权请联络XXX<br/></div>
+            <div class="fotbox2">
+                <h3>我的介绍</h3>
+                <p>·野兔，男，天秤座。<span class='myinfo_pic'><a href='http://music.163.com/#/user/home?id=42209280'
+                                                         target='_blank'><span
+                                    class='miao miao-headphones miao-fw miao-link' title='网易云音乐'></span></a><a
+                                href='http://steamcommunity.com/id/yetui' target='_blank'><span
+                                    class='miao miao-steam miao-fw miao-link' title='Steam'></span></a><a
+                                href='http://sighttp.qq.com/authd?IDKEY=5929de917711dbd2c7605f448208620a72f5a7a4af81f532'
+                                target='_blank'><span class='miao miao-qq miao-fw miao-link' title='腾讯QQ'></span></a><a
+                                href='mailto:admin@azimiao.com'><span class='miao miao-mail-alt miao-fw miao-link'
+                                                                      title='发送邮件'></span></a><span
+                                class='miao miao-guidedog miao-fw miao-link' title='占位置'></span></span></p>
+                <p>·轻微中二病患者，白学学者与棒棒伲购美病研究人员。</p>
+                <p>·泡茶，曲奇，散步，野游，微醉。</p>
+                <p>·摄影门外汉，北漂酷码农。</p>
+                <p>·梓喵出没名称的由来：<a href='//www.azimiao.com/li-shi-de-jin-cheng'>梓喵出没编年史</a></p></div>
+        </div>
+        <div class="copry clearfix">
+            <div class="maxt">
+            <span id="mt">
+                <a href="//cn.wordpress.org" rel="external" target="_blank">
+                    <i class="miao miao-wordpress miao-fw"></i>
+                </a>
+                <a href="//creativecommons.org/licenses/by-nc-sa/3.0/cn/legalcode" target="_blank">
+                    <i class="miao miao-cc miao-fw"></i>
+                </a>
+                Theme Purelove by <a href="//www.azimiao.com/purelovethemes" target="_blank">梦月酱</a>
+            </span>
+                <div id="copyright">
+                    <i class="miao miao-copyright miao-fw"></i>
+                    2014-2018 梓喵出没 | <a href="//www.azimiao.com/sitemap.html"target="_blank">[网站地图]</a> |
+                    <a href="http://www.miitbeian.gov.cn/" TARGET="_BLANK">冀ICP备15008710号</a>
+                    <a href="http://www.miitbeian.gov.cn/" TARGET="_BLANK">冀ICP备15008710号</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+<script src="<?php $this->options->themeUrl('js/purelove.js'); ?>"></script>
+<script src="<?php $this->options->themeUrl('js/responsiveslides.min.js'); ?>"></script>
+<script src='<?php $this->options->themeUrl('js/highlight.pack.js'); ?>'></script>
+<script src='<?php $this->options->themeUrl('js/wp-embed.min.js'); ?>'></script>
+<script>
+    jQuery(document).ready(function ($) {
+        //幻灯片
+        $("#slider").responsiveSlides({
+            auto: true,
+            // pager: false,
+            nav: true,
+            speed: 500,
+            pauseControls: true,
+            pager: true,
+            manualControls: "auto",
+            namespace: "slide"
+        });
+        //幻灯片导航
+        $(".mySliderBar").hover(function () {
+            $(".slide_nav").fadeIn(200)
+        }, function () {
+            $(".slide_nav").fadeOut(200)
+        });
+        function d() {
+            document.title = document[b] ? " :( 人呢? 回来" : a
+        }
+
+        var b, c, a = document.title;
+        "undefined" != typeof document.hidden ? (b = "hidden", c = "visibilitychange") : "undefined" != typeof document.mozHidden ? (b = "mozHidden", c = "mozvisibilitychange") : "undefined" != typeof document.webkitHidden && (b = "webkitHidden", c = "webkitvisibilitychange"), ("undefined" != typeof document.addEventListener || "undefined" != typeof document[b]) && document.addEventListener(c, d, !1)
+    });
+</script>
+<?php $this->footer(); ?>
