64aead5361bae0db54dd3072a13b9d971cd832c3
覃幸和 <qinxinghe@zp2.com>
2018-11-12 17:36:22 +0800

    侧边栏

--- sidebar.php
+++ sidebar.php
@@ -1,30 +1,51 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <aside class="sidebar">
-    <section class="widgetbox"><h3>嗯？</h3>
-        <p style="margin-left:auto;margin-right:auto;text-align:center;font-family:Microsoft Yahei, Helvetica, Arial, sans-serif;font-size:13px;">
-            是要<a href="#">登陆</a>呀？ </p>
+    <section class="widgetbox">
+        <h3><?php $this->options->title(); ?></h3>
+        <p class="description"><?php $this->options->description() ?></p>
     </section>
     <section class="widgetbox">
-        <h3>网站统计</h3>
-        <ul class="blogroll clearfix">
-            <li>文章总数：115篇</li>
-            <li>评论总数：673条</li>
-            <li>页面总数：15 个</li>
-            <li>分类总数：4个</li>
-            <li>标签总数：201个</li>
-            <li>网站运行：1347天</li>
+        <h3><?php _e('网站统计'); ?></h3>
+        <ul class="blogroll">
+            <?php Typecho_Widget::widget('Widget_Stat')->to($stat); ?>
+            <li><?php _e('文章总数：'); ?><?php $stat->publishedPostsNum() ?></li>
+            <li><?php _e('分类总数：'); ?><?php $stat->categoriesNum() ?></li>
+            <li><?php _e('评论总数：'); ?><?php $stat->publishedCommentsNum() ?></li>
+            <li><?php _e('页面总数：'); ?><?php $stat->publishedPagesNum() ?></li>
+            <li><?php _e('标签总数：'); ?>X</li>
+            <li><?php _e('网站运行：'); ?>X</li>
         </ul>
     </section>
-    <section class="widgetbox"><h3>友情链接</h3>
+    <section class="widgetbox">
+        <h3><?php _e('最近回复'); ?></h3>
         <div class="textwidget">
-            <ul class="friendlinks">
-                <li><a href="#" target="_blank" title="Exploring the Tao"> Pret的121号实验室</a></li>
-                <li><a href="#" target="_blank" title="Exploring the Tao"> Pret的121号实验室</a></li>
-                <li><a href="#" target="_blank" title="Exploring the Tao"> Pret的121号实验室</a></li>
-                <li><a href="#" target="_blank" title="Exploring the Tao"> Pret的121号实验室</a></li>
-                <li><a href="#" target="_blank" title="Exploring the Tao"> Pret的121号实验室</a></li>
-                <li><a href="#" style="color:red;">·查看更多OR申请</a></li>
+            <ul>
+                <?php $this->widget('Widget_Comments_Recent')->to($comments); ?>
+                <?php while($comments->next()): ?>
+                    <li><a href="<?php $comments->permalink(); ?>"><?php $comments->author(false); ?>: <?php $comments->excerpt(35, '...'); ?></a></li>
+                <?php endwhile; ?>
             </ul>
         </div>
+    </section>
+    <section class="widgetbox">
+        <h3 class="widget-title"><?php _e('归档'); ?></h3>
+        <ul>
+            <?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=Y-m-d&limit=6')
+                ->parse('<li><a href="{permalink}">{date}</a></li>'); ?>
+        </ul>
+    </section>
+    <section class="widgetbox">
+        <h3 class="widget-title"><?php _e('其它'); ?></h3>
+        <ul class="">
+            <?php if($this->user->hasLogin()): ?>
+                <li class="last"><a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a></li>
+                <li><a href="<?php $this->options->logoutUrl(); ?>"><?php _e('退出'); ?></a></li>
+            <?php else: ?>
+                <li class="last"><a href="<?php $this->options->adminUrl('login.php'); ?>"><?php _e('登录'); ?></a></li>
+            <?php endif; ?>
+            <li><a href="<?php $this->options->feedUrl(); ?>"><?php _e('文章 RSS'); ?></a></li>
+            <li><a href="<?php $this->options->commentsFeedUrl(); ?>"><?php _e('评论 RSS'); ?></a></li>
+            <li><a href="http://www.typecho.org">Typecho</a></li>
+        </ul>
     </section>
 </aside>
