286ac40183a1ef98db9033bf1aa5d56d7badd97c
覃幸和 <qinxinghe@zp2.com>
2018-11-13 16:12:53 +0800

    首页与archive页面共用同一模板

--- index.php
+++ index.php
@@ -14,12 +14,23 @@ $this->need('header.php');
 <!--主体部分-->
 <section id="container">
     <section id="content">
-        <div class="mySliderBar">
-            <ul class="rslides" id="slider">
-                <li><img src='<?php $this->options->themeUrl('images/tmp68686407_p0.jpg'); ?>'></li>
-                <li><img src='<?php $this->options->themeUrl('images/tmp68577569_p0_master1200.jpg'); ?>'></li>
-            </ul>
-        </div>
+        <?php if ($this->is('index')):?>
+            <div class="mySliderBar">
+                <ul class="rslides" id="slider">
+                    <li><img src='<?php $this->options->themeUrl('images/tmp68686407_p0.jpg'); ?>'></li>
+                    <li><img src='<?php $this->options->themeUrl('images/tmp68577569_p0_master1200.jpg'); ?>'></li>
+                </ul>
+            </div>
+        <?php else:?>
+            <h3 class="archive-title">
+                <?php $this->archiveTitle(array(
+                    'category'  =>  _t('分类 %s 下的文章'),
+                    'search'    =>  _t('包含关键字 %s 的文章'),
+                    'tag'       =>  _t('标签 %s 下的文章'),
+                    'author'    =>  _t('%s 发布的文章')
+                ), '', ''); ?>
+            </h3>
+        <?php endif;?>
         <?php while ($this->next()): ?>
             <article class="posts" itemscope itemtype="http://schema.org/BlogPosting">
                 <div class="label">
@@ -55,6 +66,7 @@ $this->need('header.php');
                 </div>
             </article>
         <?php endwhile; ?>
+    <?php $this->pageNav('&laquo; 前一页', '后一页 &raquo;', null, null, ['wrapTag' => 'div', 'wrapClass' => 'pagenavi']); ?>
     </section><!-- #content -->
     <?php $this->need('sidebar.php'); ?>
     <?php $this->need('footer.php'); ?>
