f1c15bb21b39b312d6a552e3c989989fd3edd1b4
覃幸和 <qinxinghe@zp2.com>
2018-11-13 16:14:12 +0800

    文章页面

---
+++ post.php
@@ -0,0 +1,55 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php $this->need('header.php'); ?>
+
+<!--主体部分-->
+<section id="container">
+    <section id="content">
+        <article id="article" itemscope itemtype="http://schema.org/BlogPosting">
+            <header class="entry-header">
+                <h1 class="entry-title" itemprop="name headline" itemtype="http://schema.org/Article">
+                    <?php $this->title() ?>
+                </h1>
+                <div class="entry-meta">
+                    <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time> /
+                    <span itemprop="category"><?php $this->category(','); ?></span> /
+                    <span itemprop="author" itemscope itemtype="http://schema.org/Person">
+                        <a itemprop="name" class="fn" href="<?php $this->author->permalink(); ?>" rel="author">
+                            <?php $this->author(); ?>
+                        </a>
+                    </span>
+                    <span class="breadcrumb" itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
+                        <span>
+                            <span>位置：</span>
+                            <a href="<?php $this->options->siteUrl(); ?>">Home</a> »
+                            <?php $this->category(); ?> »
+                            <a itemprop="url" href="<?php $this->permalink() ?>" rel="bookmark">本页</a>
+                        </span>
+                    </span>
+                </div>
+            </header>
+            <div class="entry-content" itemtype="http://schema.org/Article" itemprop="articleBody">
+                <?php $this->content(); ?>
+            </div>
+            <div id="heart" style="text-align: center;"><embed src="https://www.azimiao.com/wp-content/themes/purelove/images/heart1.svg" width="64" height="64" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/"></div>
+
+            <span class="poststags clearfix" itemprop="tags">
+                <?php $this->tags('', true, 'none'); ?>
+            </span>
+            <div id="related">
+                <h3 class="coms_underline">你可能也喜欢</h3>
+                <?php $this->related(6)->to($relatedPosts); ?>
+                <ul>
+                    <?php while ($relatedPosts->next()): ?>
+                        <li>
+                            <a href="<?php $relatedPosts->permalink(); ?>" rel="bookmark" title="<?php $relatedPosts->title(); ?>">
+                                <?php $relatedPosts->title(26); ?>
+                            </a>
+                        </li>
+                    <?php endwhile; ?>
+                </ul>
+            </div>
+        <?php $this->need('comments.php'); ?>
+        </article>
+    </section>
+<?php $this->need('sidebar.php'); ?>
+<?php $this->need('footer.php'); ?>
