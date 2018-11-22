56467ad9e3b509dbd1319ae25aba6c8f1897e103
覃幸和 <qinxinghe@zp2.com>
2018-11-22 10:14:50 +0800

    自定义归档页

--- articles.php
+++ articles.php
@@ -2,7 +2,8 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 $this->need('header.php');
 /**
- * 归档页面
+ * 自定义归档页
+ * @package custom
  * @link http://docs.typecho.org/themes/custom-theme#%E8%87%AA%E5%AE%9A%E4%B9%89%E9%A1%B5%E9%9D%A2_page_%E6%A8%A1%E6%9D%BF
  */
 ?>
