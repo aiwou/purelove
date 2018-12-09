f61326e31717bb09c758ca6ff473a96fb796d379
Hoe <qinxinghe@zp2.com>
2018-12-09 15:51:08 +0800

    适应移动设备

--- sidebar.php
+++ sidebar.php
@@ -93,26 +93,30 @@
     <?php if ($this->options->sidebarBlock && in_array('showArchive', $this->options->sidebarBlock)): ?>
         <section class="widgetbox">
             <h3 class="widget-title"><?php _e('归档'); ?></h3>
-            <ul>
-                <?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=Y-m-d&limit=6')
-                    ->parse('<li><a href="{permalink}">{date}</a></li>'); ?>
-            </ul>
+            <div class="textwidget">
+                <ul>
+                    <?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=Y-m-d&limit=6')
+                        ->parse('<li><a href="{permalink}">{date}</a></li>'); ?>
+                </ul>
+            </div>
         </section>
     <?php endif; ?>
     <?php if ($this->options->sidebarBlock && in_array('showOther', $this->options->sidebarBlock)): ?>
         <section class="widgetbox">
             <h3 class="widget-title"><?php _e('其它'); ?></h3>
-            <ul class="">
-                <?php if($this->user->hasLogin()): ?>
-                    <li class="last"><a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a></li>
-                    <li><a href="<?php $this->options->logoutUrl(); ?>"><?php _e('退出'); ?></a></li>
-                <?php else: ?>
-                    <li class="last"><a href="<?php $this->options->adminUrl('login.php'); ?>"><?php _e('登录'); ?></a></li>
-                <?php endif; ?>
-                <li><a href="<?php $this->options->feedUrl(); ?>"><?php _e('文章 RSS'); ?></a></li>
-                <li><a href="<?php $this->options->commentsFeedUrl(); ?>"><?php _e('评论 RSS'); ?></a></li>
-                <li><a href="http://www.typecho.org">Typecho</a></li>
-            </ul>
+            <div class="textwidget">
+                <ul class="">
+                    <?php if($this->user->hasLogin()): ?>
+                        <li class="last"><a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a></li>
+                        <li><a href="<?php $this->options->logoutUrl(); ?>"><?php _e('退出'); ?></a></li>
+                    <?php else: ?>
+                        <li class="last"><a href="<?php $this->options->adminUrl('login.php'); ?>"><?php _e('登录'); ?></a></li>
+                    <?php endif; ?>
+                    <li><a href="<?php $this->options->feedUrl(); ?>"><?php _e('文章 RSS'); ?></a></li>
+                    <li><a href="<?php $this->options->commentsFeedUrl(); ?>"><?php _e('评论 RSS'); ?></a></li>
+                    <li><a href="http://www.typecho.org">Typecho</a></li>
+                </ul>
+            </div>
         </section>
     <?php endif; ?>
 </aside>
